7cf5a242354215ce5b99a773896a6510006d7aa8
Madio Seck <madiomadio@hotmail.com>
2022-04-25 10:07:55 +0200

    code works, value reference and other issues fixed

--- part2/nuxVueTest/components/CreateTable.vue
+++ part2/nuxVueTest/components/CreateTable.vue
@@ -1,43 +1,65 @@
 <template>
-    <div>
-        
-
-<table id="firstTable">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Phone</th>
-      <th>Profession</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="row in rows" v-bind:key="row.id">
-      <td>{{row.id}}</td>
-      <td>{{row.name}}</td>
-      <td>{{row.phone}}</td>
-      <td>{{row.profession}}</td>
-    </tr>
-  </tbody>
-</table>
-
-
-    </div>
+  <div>
+    <table id="firstTable">
+      <thead>
+        <tr>
+          <th>ID:</th>
+          <th>Assignee:</th>
+          <th>Due Date and Time:</th>
+          <th>Description:</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in myData" v-bind:key="row.id">
+          <!--the data is saved as an array of objects in the state, so to reference the values,
+          the object first needs to be reached and the the name of the key called-->
+          <td>{{Object.values(row)[0].id}}</td>
+          <td>{{Object.values(row)[0].assignee}}</td>
+          <td>{{Object.values(row)[0].dueDateTime}}</td>
+          <td>{{Object.values(row)[0].description}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    import Vue from 'vue'
-    var firstTable = new Vue({
-    el: '#firstTable',
-    data: {
-        rows: [
-        { id: 1, name: "Chandler Bing", phone: '305-917-1301', profession: 'IT Manager' },
-        { id: 2, name: "Ross Geller", phone: '210-684-8953', profession: 'Paleontologist' },
-        { id: 3, name: "Rachel Green", phone: '765-338-0312', profession: 'Waitress'},
-        { id: 4, name: "Monica Geller", phone: '714-541-3336', profession: 'Head Chef' },
-        { id: 5, name: "Joey Tribbiani", phone: '972-297-6037', profession: 'Actor' },
-        { id: 6, name: "Phoebe Buffay", phone: '760-318-8376', profession: 'Masseuse' }
-        ]
+  export default {
+    data() {
+        return {
+            myData: this.$store.state.myToDos
+        }
     }
-    });
+  }
 </script>
+
+<style>
+  table {
+  font-family: 'Open Sans', sans-serif;
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin: 10px 10px 0 10px;
+  }
+
+  table th {
+    text-transform: uppercase;
+    text-align: left;
+    background: #44475C;
+    color: #FFF;
+    padding: 8px;
+    min-width: 30px;
+  }
+
+  table td {
+    text-align: left;
+    padding: 8px;
+    border-right: 2px solid #7D82A8;
+  }
+  table td:last-child {
+    border-right: none;
+  }
+  table tbody tr:nth-child(2n) td {
+    background: #D4D8F9;
+  }
+</style>
--- part2/nuxVueTest/pages/mytable.vue
+++ part2/nuxVueTest/pages/mytable.vue
@@ -24,8 +24,6 @@
         <MyNavigator/>
         <h1>Here are all the todo-items</h1>
         <button v-on:click="callToDo" >Get more data</button>
-        <p>{{result}}</p>
-        <p>{{this.$store.state.myToDos}}</p>
         <CreateTable/>
     </div>
 </template>
@@ -42,25 +40,6 @@ export default {
         callToDo: function() {
             this.$store.dispatch("getToDos");
             //this.makeTable(this.$store.state.myToDos);
-        },
-        makeTable: function(tableData){
-            var table = document.createElement('table');
-            var tableBody = document.createElement('tbody');
-
-            tableData.forEach(function(rowData) {
-                var row = document.createElement('tr');
-
-                rowData.forEach(function(cellData) {
-                var cell = document.createElement('td');
-                cell.appendChild(document.createTextNode(cellData));
-                row.appendChild(cell);
-                });
-
-                tableBody.appendChild(row);
-            });
-
-            table.appendChild(tableBody);
-            document.body.appendChild(table);
         }
     },
     computed: {
@@ -70,24 +49,3 @@ export default {
     }
 }
 </script>
-<!--
-Plan of attack:
-- make button click function call 
-- make function call API
-- 
-
-data comes in like this;
-{ "todo": {
-    "id": "8968d7ee-0c5e-4838-af7b-9a9cdc6d9184", 
-    "assignee": "David", 
-    "dueDateTime": "2022-04-30T16:00:00.000Z", 
-    "description": "Levarne Organogram Graphic maken" 
-    } 
-}
-
-example;
-var list = ["abe", "bobbo", "chrisly"]
-each item in list
-    li = item
-</template>
--->
